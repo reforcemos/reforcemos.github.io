55d298305a897fef1296ae426895231868b759aa
reforcemos <lucasjax911@gmail.com>
2018-03-26 12:16:17 -0300

    commitando tudo

--- js/fadein.js
+++ js/fadein.js
@@ -1,3 +1,5 @@
+
+/*Fade in o que oferecemos*/
 $(document).ready(function() {
 
     /* Every time the window is scrolled ... */
@@ -5,6 +7,33 @@ $(document).ready(function() {
 
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+                $('.hideme2').each(function(i){
+
+                });
+            }
+
+        });
+
+    });
+
+});
+
+/*Fadein do conheca-nos*/
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hideme-text').each( function(i){
 
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
